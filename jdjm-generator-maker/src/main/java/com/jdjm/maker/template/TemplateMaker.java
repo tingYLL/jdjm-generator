0ec932e78ff5048e0223ae89e279f215d525a46b
ZhengHongyi <111111@163.com>
2024-07-07 10:28:43 +0800

    增加逻辑： 1.每次生成模板的时候判断当前是否为第一次生成，如果不是第一次生成就继续补充上次的ftl模板 2.fileInfoList去重

--- jdjm-generator-maker/src/main/java/com/jdjm/maker/template/TemplateMaker.java
+++ jdjm-generator-maker/src/main/java/com/jdjm/maker/template/TemplateMaker.java
@@ -12,38 +12,38 @@ import com.jdjm.maker.meta.enums.FileTypeEnum;
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TemplateMaker {
 
-    public static void main(String[] args) {
-        //这里会在当前的maker目录下生成一个临时的目录
-        //指定原始项目路径
+    private static long makeTemplate(Long id){
+        //没有id则生成
+        if(id == null){
+            id = IdUtil.getSnowflakeNextId();
+        }
+        //业务逻辑...
         String projectPath = System.getProperty("user.dir");
+        //指定原始项目路径
         String originProjectPath = FileUtil.getAbsolutePath(new File(projectPath).getParentFile()) + File.separator + "jdjm-generator-demo-projects/acm-template-pro";
 
-        //复制目录
-        long id = IdUtil.getSnowflakeNextId();
         String tempDirPath = projectPath + File.separator + ".temp";
         String templatePath = tempDirPath + File.separator + id;
 
         if(!FileUtil.exist(templatePath)){
             FileUtil.mkdir(templatePath);
+            //在当前的maker目录下的.tmp文件夹下生成一个临时文件夹
+            FileUtil.copy(originProjectPath,templatePath,true);
         }
 
-        FileUtil.copy(originProjectPath,templatePath,true);
-
-        //一、输入信息
-        //1.项目的基本信息
         String name = "acm-template-generator";
         String description = "ACM 示例模板生成器";
 
-        //2.输入文件信息
-        //拼接出完整的要挖坑的项目根目录，这里直接拿当前maker项目里的。注意要记得拼上originProjectPath路径下最后一层的文件夹的名称
+        //拼接出完整目录，注意要记得拼上originProjectPath路径下最后一层的文件夹的名称
         String sourceRootPath = templatePath + File.separator + FileUtil.getLastPathEle(Paths.get(originProjectPath)).toString();
         sourceRootPath = sourceRootPath.replace("\\","/");
         //或者sourceRootPath = sourceRootPath.replaceAll("\\\\","/");
-        //要挖坑的文件
         String fileInputPath = "src/com/jdjm/acm/MainTemplate.java";
         String fileOutpuhPath = fileInputPath + ".ftl";
 
@@ -53,44 +53,81 @@ public class TemplateMaker {
         modelInfo.setType("String");
         modelInfo.setDefaultValue("sum = ");
 
-        //二、 使用字符串替换，生成模板文件
         String fileInputAbsolutePath = sourceRootPath + File.separator + fileInputPath;
-        String fileContent = FileUtil.readUtf8String(fileInputAbsolutePath);
+        String fileOutputAbsolutePath =  sourceRootPath + File.separator +fileOutpuhPath;
+        String fileContent;
+        //如果有模板文件，表示不是第一次制作，因此应该继续在原有的模板基础上继续挖坑
+        if(FileUtil.exist(fileOutputAbsolutePath)){
+            fileContent = FileUtil.readUtf8String(fileOutputAbsolutePath);
+        }else{
+            fileContent = FileUtil.readUtf8String(fileInputAbsolutePath);
+        }
         String replacement = String.format("${%s}",modelInfo.getFieldName());
         String newFileContent = StrUtil.replace(fileContent,"Sum: ",replacement);
-
         //输出模板文件
-        String fileOutputAbsolutePath =  sourceRootPath + File.separator +fileOutpuhPath;
         FileUtil.writeUtf8String(newFileContent,fileOutputAbsolutePath);
 
-        //三、生成配置文件
-        String metaOutputPath = sourceRootPath + File.separator + "meta.json";
 
-        //1.构造文件信息 （fileConfig）
-        Meta meta = new Meta();
-        meta.setName(name);
-        meta.setDescription(description);
-        Meta.FileConfig fileConfig = new Meta.FileConfig();
-        meta.setFileConfig(fileConfig);
-        fileConfig.setSourceRootPath(sourceRootPath);
-        List<Meta.FileConfig.FileInfo> fileInfoList = new ArrayList<>();
-        fileConfig.setFiles(fileInfoList);
         Meta.FileConfig.FileInfo fileInfo = new Meta.FileConfig.FileInfo();
         fileInfo.setInputPath(fileInputPath);
         fileInfo.setOutputPath(fileOutpuhPath);
         fileInfo.setType(FileTypeEnum.FILE.getValue());
         fileInfo.setGenerateType(FileGenerateTypeEnum.DYNAMIC.getValue());
-        fileInfoList.add(fileInfo);
 
-        //2.构造模型信息 (modelConfig)
-        Meta.ModelConfig modelConfig = new Meta.ModelConfig();
-        meta.setModelConfig(modelConfig);
-        List<Meta.ModelConfig.ModelInfo> modelInfoList = new ArrayList<>();
-        modelInfoList.add(modelInfo);
-        modelConfig.setModels(modelInfoList);
+        //配置文件
+        String metaOutputPath = sourceRootPath + File.separator + "meta.json";
 
-        // 通过对象生成格式化好的字符串，即格式化好的meta.json
-        String s = JSONUtil.toJsonPrettyStr(meta);
-        FileUtil.writeUtf8String(s,metaOutputPath);
+        //如果已有meta.json，说明不是第一次制作，应该在原有文件上继续修改
+        if(FileUtil.exist(metaOutputPath)){
+            //先以字符串的形式读取meta.json，后转成对象
+            Meta oldMeta = JSONUtil.toBean(FileUtil.readUtf8String(metaOutputPath),Meta.class);
+            //追加参数配置
+            List<Meta.FileConfig.FileInfo> fileInfoList = oldMeta.getFileConfig().getFiles();
+            fileInfoList.add(fileInfo);
+            List<Meta.ModelConfig.ModelInfo> modelInfoList = oldMeta.getModelConfig().getModels();
+            modelInfoList.add(modelInfo);
+            //输出meta.json元信息文件
+            FileUtil.writeUtf8String(JSONUtil.toJsonPrettyStr(oldMeta),metaOutputPath);
+        }else{
+            //1.构造文件信息 （fileConfig）
+            Meta meta = new Meta();
+            meta.setName(name);
+            meta.setDescription(description);
+            Meta.FileConfig fileConfig = new Meta.FileConfig();
+            meta.setFileConfig(fileConfig);
+            fileConfig.setSourceRootPath(sourceRootPath);
+            List<Meta.FileConfig.FileInfo> fileInfoList = new ArrayList<>();
+            fileConfig.setFiles(fileInfoList);
+//            Meta.FileConfig.FileInfo fileInfo = new Meta.FileConfig.FileInfo();
+//            fileInfo.setInputPath(fileInputPath);
+//            fileInfo.setOutputPath(fileOutpuhPath);
+//            fileInfo.setType(FileTypeEnum.FILE.getValue());
+//            fileInfo.setGenerateType(FileGenerateTypeEnum.DYNAMIC.getValue());
+            fileInfoList.add(fileInfo);
+
+            //2.构造模型信息 (modelConfig)
+            Meta.ModelConfig modelConfig = new Meta.ModelConfig();
+            meta.setModelConfig(modelConfig);
+            List<Meta.ModelConfig.ModelInfo> modelInfoList = new ArrayList<>();
+            modelInfoList.add(modelInfo);
+            modelConfig.setModels(modelInfoList);
+
+            // 通过对象生成格式化好的字符串，即格式化好的meta.json
+            String s = JSONUtil.toJsonPrettyStr(meta);
+            FileUtil.writeUtf8String(s,metaOutputPath);
+        }
+
+        return id;
+    }
+
+    //对FileInfoList进行去重
+    private static List<Meta.FileConfig.FileInfo> distinctFiles(List<Meta.FileConfig.FileInfo> fileInfoList){
+        Collection<Meta.FileConfig.FileInfo> values = fileInfoList.stream().collect(Collectors.toMap(Meta.FileConfig.FileInfo::getInputPath, o -> o, (e, r) -> r)).values();
+        List<Meta.FileConfig.FileInfo> newFileInfoList = new ArrayList<>(values);
+        return newFileInfoList;
+    }
+
+    public static void main(String[] args) {
+        TemplateMaker.makeTemplate(1808886735329832960L);
     }
 }
